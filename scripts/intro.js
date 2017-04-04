371780f00291c324a603e982a438493d9f926a24
prajankya <prajankya@gmail.com>
2017-04-04 13:51:02 +0530

    updated intro

--- scripts/intro.js
+++ scripts/intro.js
@@ -3,9 +3,17 @@
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
+const art = require('ascii-art');
+const pictureTube = require('picture-tube')
 const debug = require('debug')("snappy:core:intro")
 
 module.exports = function() {
+  var image = path.join(__dirname, '..', 'public', 'images', 'logo_white.png')
+
+  var tube = pictureTube()
+  tube.pipe(process.stdout)
+
+
   var interfaces = os.networkInterfaces()
   var addresses = []
   for (var k in interfaces) {
@@ -21,31 +29,25 @@ module.exports = function() {
       }
     }
   }
-  // =========================================================================
-  global.snappy_core.package = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "package.json")))
 
-  debug("==========================================================================")
-  debug("\t\t\t\t\t" + global.snappy_core.package.name)
-  debug("\t\t" + global.snappy_core.package.description)
-  debug("\t\t\t\t       version:" + global.snappy_core.package.version)
-  debug("\t\t\t\tServer Running on Port : " + global.snappy_core.PORT)
-
-  for (var i = 0; i < addresses.length; i++) {
-    debug("\t\t\t\t   Listening on : " + addresses[i].address + ":" + global.snappy_core.PORT)
-  }
-
-  debug("==========================================================================")
-
-  const art = require('ascii-art');
-  var image = path.join(__dirname, '..', 'public', 'images', 'logo_white.png')
-
-  var pictureTube = require('picture-tube')
-  var tube = pictureTube()
-  tube.pipe(process.stdout)
+  const myPackage = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "package.json")))
 
 
   art.font('Snappy Robotics', 'Doom', 'cyan', function(ascii) {
-    console.log(ascii);
+    fs.createReadStream(image)
+      .pipe(tube)
+      .on('end', function() {
+        console.log(ascii);
+        debug("==========================================================================")
+        debug("\t\t\t\t" + myPackage.name)
+        debug("\t" + myPackage.description)
+        debug("\t\t\t       version:" + myPackage.version)
+        debug("\t\t\tServer Running on Port : " + global.snappy_core.PORT)
+
+        for (var i = 0; i < addresses.length; i++) {
+          debug("\t\t\t   Listening on : " + addresses[i].address + ":" + global.snappy_core.PORT)
+        }
+        debug("==========================================================================")
+      });
   })
-  fs.createReadStream(image).pipe(tube);
 }
