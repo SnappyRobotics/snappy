b07d23725c98ab1b80975d334460dbd35684dad6
prajankya <prajankya@gmail.com>
2017-04-06 15:54:24 +0530

    cleaned index

--- index.js
+++ index.js
@@ -13,28 +13,23 @@ require(_.path('scripts', 'intro'))()
 
 
 //---------------------------Node-RED-----------------------------------------
-const red_connector = require(path.join(__dirname, 'scripts', 'red_connector'));
+const red_connector = require(path.join(__dirname, 'scripts', 'red_connector'))
 
-
-/*
-global.snappy_core.stop = red_connector.stop_red
-global.snappy_core.start = red_connector.start_red
-global.snappy_core.clean =
-*/
 if (require.main === module) {
   red_connector.init()
 
   red_connector.server.on('error', function(e) {
     debug("Cannot start server as Port is being used :", global.snappy_core.PORT)
 
-    console.error(e);
+    console.error(e)
 
     process.exit(1)
   })
 
   red_connector.start_red()
 } else {
-  debug('required as a module');
+  debug('required as a module')
+
   module.exports = {
     start: red_connector.start_red,
     stop: red_connector.stop_red,
@@ -47,9 +42,9 @@ if (require.main === module) {
           return _.createConfig()
         })
         .then(function() {
-          debug("Cleaned");
+          debug("Cleaned")
           return
-        });
+        })
     }
   }
 }
