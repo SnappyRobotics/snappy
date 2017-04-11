760d3d8cca7e8f0ed6f8cd4e83c910f7f995115d
prajankya <prajankya@gmail.com>
2017-04-11 16:18:52 +0530

    updated full internal settings of node-red

--- data/red-settings.js
+++ data/red-settings.js
@@ -1,7 +1,7 @@
 const _ = require(global.initLocation)
 
 module.exports = {
-  httpAdminRoot: "/red",
+  httpAdminRoot: "/",
   httpNodeRoot: "/api",
   userDir: _.path("userDir", "red"),
   functionGlobalContext: {}, // enables global context,
@@ -16,8 +16,7 @@ module.exports = {
     'analysis',
     'advanced'
   ],
-  logging: 'fatal',
-
+  // logging: 'fatal',
   editorTheme: {
     page: {
       title: "Snappy Robotics",
@@ -31,22 +30,27 @@ module.exports = {
     },
     deployButton: {
       type: "simple",
-      label: "Run",
-      icon: "/absolute/path/to/deploy/button/image" // or null to remove image
+      label: "Deploy",
+      icon: null
     },
     menu: { // Hide unwanted menu items by id. see editor/js/main.js:loadEditor for complete list
+      // "menu-item-view-menu": false,
+      "menu-item-import": false,
+      "menu-item-export": false,
       "menu-item-import-library": false,
       "menu-item-export-library": false,
+      "menu-item-show-tips": false,
+      "menu-item-search": false,
+      "menu-item-config-nodes": false,
+      "menu-item-workspace": false,
+      "menu-item-subflow": false,
+      "menu-item-node-red-version": false,
       "menu-item-keyboard-shortcuts": false,
+      "menu-item-edit-palette": false,
       "menu-item-help": {
         label: "View Help Online",
         url: "https://SnappyRobotics.github.io/docs/backend"
       }
-    },
-    userMenu: false, // Hide the user-menu even if adminAuth is enabled
-    login: {
-      image: "/absolute/path/to/login/page/big/image" // a 256x256 image
     }
-  },
-
+  }
 }
